5590ba689b523864c65befbe630f1c2f8c51267c
rong <519247349@qq.com>
2020-07-15 13:17:39 +0800

    feature1.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("222222222222");
+        System.out.println("3333333333");
     }
 
 }
